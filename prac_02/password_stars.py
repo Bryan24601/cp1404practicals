4487769b85e542ab47fb445c1716e22a37c96674
Bryan Ong Jing Rong 14355339 <bryanjingrong.ong@my.jcuedu.au>
2023-03-20 10:44:27 +0800

    final password check program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,20 @@
-minimum_length = 8
-password = str(input("Password: "))
-while len(password) < minimum_length:
-    print("Invalid Password Length")
+def main():
+    minimum_length = 8
+    password = get_password(minimum_length)
+    asterisk_printing(password)
+
+
+def asterisk_printing(password):
+    for i in password:
+        print("*", end='')
+
+
+def get_password(minimum_length):
     password = str(input("Password: "))
-for i in password:
-    print("*", end='')
+    while len(password) < minimum_length:
+        print("Invalid Password Length")
+        password = str(input("Password: "))
+    return password
+
+
+main()
