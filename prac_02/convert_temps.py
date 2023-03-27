a85bc97a58b47c0c4c13d787d2b39e8e76d6585e
Bryan Ong Jing Rong 14355339 <bryanjingrong.ong@my.jcuedu.au>
2023-03-27 09:30:22 +0800

    updated prac 2

---
+++ prac_02/convert_temps.py
@@ -0,0 +1,23 @@
+temps_input_file = open("temps_input", "r")
+temps_output_file = open("temps_output", "a")
+
+
+def main():
+    for current_line in temps_input_file:
+        new_fahrenheit = float(current_line)
+        new_celcius = fahrenheit_to_celsius(new_fahrenheit)
+        print(f"{new_celcius}")
+        temps_output_file.write(f"{new_celcius}\n")
+
+
+def celsius_to_fahrenheit(celsius):
+    new_fahrenheit = celsius * 9.0 / 5 + 32
+    return new_fahrenheit
+
+
+def fahrenheit_to_celsius(fahrenheit):
+    new_celsius = 5 / 9 * (fahrenheit - 32)
+    return new_celsius
+
+
+main()
--- prac_02/temperature_writing.py
+++ prac_02/temperature_writing.py
@@ -1,23 +1,7 @@
-temps_input_file = open("temps_input", "r")
-temps_output_file = open("temps_output", "a")
-
-
-def main():
-    for current_line in temps_input_file:
-        new_fahrenheit = float(current_line)
-        new_celcius = fahrenheit_to_celsius(new_fahrenheit)
-        print(f"{new_celcius}")
-        temps_output_file.write(f"{new_celcius}\n")
-
-
-def celsius_to_fahrenheit(celsius):
-    new_fahrenheit = celsius * 9.0 / 5 + 32
-    return new_fahrenheit
-
-
-def fahrenheit_to_celsius(fahrenheit):
-    new_celsius = 5 / 9 * (fahrenheit - 32)
-    return new_celsius
-
-
-main()
+import random
+temperature_write_file = open("temps_input", "a")
+number_of_temperatures = 15
+for current_temperature in range(1, number_of_temperatures + 1):
+    new_temperature = random.randint(-200, 201)
+    print(new_temperature)
+    temperature_write_file.write(f"{new_temperature}\n")
