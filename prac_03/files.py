5851cf42495955aa8e4b02c4f604349cb30f9139
Bryan Ong Jing Rong 14355339 <bryanjingrong.ong@my.jcuedu.au>
2023-03-27 11:54:28 +0800

    prac_03 current

--- prac_03/files.py
+++ prac_03/files.py
@@ -4,13 +4,34 @@ def read():
     name_file.write(name)
     name_file.close()
 
+
 def readline():
     name_file = open("name", "r")
-    for name in name_file:
-        print(f"Your name is {name}")
+    name = name_file.readline()
+    print(f"Your name is {name}")
+
 
 def readlines():
+    numbers_file = open("numbers", "r")
+    numbers = numbers_file.readlines()
+    first_number = int(numbers[0])
+    second_number = int(numbers[1])
+    total_result = first_number + second_number
+    print(total_result)
+    numbers_file.close()
+
+
+def for_line_in_file():
+    numbers_file = open("numbers", "r")
+    total = 0
+    for current_line in numbers_file:
+        number_to_add = int(current_line)
+        total += number_to_add
+    print(total)
+    numbers_file.close()
 
 
 read()
 readline()
+readlines()
+for_line_in_file()
