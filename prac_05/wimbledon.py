4e2b832e5f8ea5cf6dbbae37fed40423cd24d5da
Bryan Ong Jing Rong 14355339 <bryanjingrong.ong@my.jcuedu.au>
2023-04-10 11:55:21 +0800

    prac05_feedback

--- prac_05/wimbledon.py
+++
@@ -1,9 +0,0 @@
-import csv
-from collections import Counter
-wimbledon_file = open("wimbledon.csv", "r", encoding="utf-8-sig")
-reader = csv.reader(wimbledon_file)
-
-print("Wimbledon Champions:\n")
-next(reader)
-champion_counts = Counter(current_row[3] for current_row in reader)
-print(champion_counts)
