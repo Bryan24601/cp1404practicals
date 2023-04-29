0bab177612be9eb39a974c70327a7332c5fe1282
Bryan Ong Jing Rong 14355339 <bryanjingrong.ong@my.jcuedu.au>
2023-04-29 10:51:13 +0800

    completed everything in myguitars.py

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -14,14 +14,27 @@ class Guitar:
         return self.year < other.year
 
 
-guitar_file = open('guitars.csv', 'r')
+guitar_file = 'guitars.csv'
 guitar_list = []
+with open(guitar_file, 'r') as guitar_file:
+    guitar_file.readline()
+    for line in guitar_file:
+        parts = line.strip().split(',')
+        current_guitar = Guitar(parts[0], parts[1], parts[2])
+        guitar_list.append(current_guitar)
+    sorted_guitar_list = sorted(guitar_list)
+    for guitar in sorted_guitar_list:
+        print(guitar)
 
-guitar_file.readline()
-for line in guitar_file:
-    parts = line.strip().split(',')
-    current_guitar = Guitar(parts[0], parts[1], parts[2])
-    guitar_list.append(current_guitar)
-sorted_guitar_list = sorted(guitar_list)
-for guitar in sorted_guitar_list:
-    print(guitar)
+
+def new_guitar(guitar_file):
+    new_guitar_name = input("Name: ")
+    while new_guitar_name != "":
+        new_guitar_year = input("Year: ")
+        new_guitar_cost = input("Cost: $")
+        add_new_guitar = Guitar(new_guitar_name, new_guitar_year, new_guitar_cost)
+        guitar_list.append(add_new_guitar)
+        new_guitar_name = input("Name: ")
+    with open(guitar_file, 'w') as guitar_file:
+        for current_guitar in guitar_list:
+            guitar_file.write(current_guitar)
